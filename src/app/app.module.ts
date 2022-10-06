f803846f06c08b4d8b36571cc5164ebb1b29eb49
Sandra <dtena90@gmail.com>
2022-10-06 17:22:26 +0200

    Crear el proyecto de 0. y pasar a la versión 14.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,16 @@
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-
-
+    AppComponent
   ],
   imports: [
-    BrowserAnimationsModule,
-
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
