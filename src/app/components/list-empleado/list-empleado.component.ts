99fa54b421a0db9baa63f6e3596137854cd17c22
Sandra <dtena90@gmail.com>
2022-10-10 10:28:50 +0200

    Renombrar el nombre del componente en el selector[list-empleado]

--- src/app/components/list-empleado/list-empleado.component.ts
+++ src/app/components/list-empleado/list-empleado.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-list-empleado',
+  selector: 'list-empleado',
   templateUrl: './list-empleado.component.html',
   styleUrls: ['./list-empleado.component.css']
 })
