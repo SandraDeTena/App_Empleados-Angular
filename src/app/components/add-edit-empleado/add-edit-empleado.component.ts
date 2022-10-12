1150ad6fa32070a3bc9e7a73403ca12ea9b79029
Sandra <dtena90@gmail.com>
2022-10-12 18:33:44 +0200

    1º Parte del Formulario, sólo dando estilos de MatForm

--- src/app/components/add-edit-empleado/add-edit-empleado.component.ts
+++ src/app/components/add-edit-empleado/add-edit-empleado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatCalendarCellClassFunction } from '@angular/material/datepicker';
 
 @Component({
   selector: 'add-edit-empleado',
@@ -6,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-edit-empleado.component.css']
 })
 export class AddEditEmpleadoComponent implements OnInit {
+  dateClass: MatCalendarCellClassFunction<Date> = (cellDate: { getDate: () => any; }, view: string) => {
+    // Only highligh dates inside the month view.
+    if (view === 'month') {
+      const date = cellDate.getDate();
 
+      // Highlight the 1st and 20th day of each month.
+      return date === 1 || date === 20 ? 'example-custom-date-class' : '';
+    }
+
+    return '';
+  };
+
+  //Estados civiles
+  estadosCiviles: any[] = ['Soltero/a', 'Casado/a', 'Viudo/a', 'Divorciado/a']
   constructor() { }
 
   ngOnInit(): void {
